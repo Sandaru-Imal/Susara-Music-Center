823b2a8b5792dd6da9a8b4a45a1b7ffc346df3b0
sandaru imal <sandaruimal99@gmail.com>
2019-09-03 21:12:54 +0530

    updated user profile

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -1,16 +1,16 @@
 <?php
 
-// namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\Auth;
 
-// use App\Http\Controllers\Controller;
-// use Illuminate\Foundation\Auth\AuthenticatesUsers;
-// use Illuminate\Http\Request;
-// use Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Auth;
 
-// class AdminLoginController extends Controller
-// {
-//     protected $table = 'admin';
-//     protected $guard = 'admin';
+class AdminLoginController extends Controller
+{
+    protected $table = 'admin';
+    protected $guard = 'admin';
     /*
     |--------------------------------------------------------------------------
     | Login Controller
@@ -22,47 +22,47 @@
     |
     */
 
-    // use AuthenticatesUsers;
+    use AuthenticatesUsers;
 
     /**
      * Where to redirect users after login.
      *
-    //  * @var string
+     * @var string
      */
     // protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
      *
-    //  * @return void
+     * @return void
      */
-    // public function __construct()
-    // {
-    //     $this->middleware('guest:admin')->except('logout');
-    // }
+    public function __construct()
+    {
+        $this->middleware('guest:admin')->except('logout');
+    }
 
-    // public function redirectTo(){
-    //     return '/front/home';
-    // }
+    public function redirectTo(){
+        return '/front/home';
+    }
 
-    // public function showLoginForm(){
-    //     return view('auth.admin-login');
-    // }
+    public function showLoginForm(){
+        return view('auth.admin-login');
+    }
 
-    // public function login(Request $request){
-    //     $this->validate($request,[
-    //         'email'=>'required|email',
-    //         'password'=>'required|min:6'
+    public function login(Request $request){
+        $this->validate($request,[
+            'email'=>'required|email',
+            'password'=>'required|min:6'
 
-    //     ]);
+        ]);
 
-        // if (Auth::guard('admin')->attempt(['email'=>$request->email,'password'=>$request->password],$request->remember)) {
-        //     return redirect()->intended('admin.dashboard');
-        // }
+        if (Auth::guard('admin')->attempt(['email'=>$request->email,'password'=>$request->password],$request->remember)) {
+            return redirect()->intended('admin.dashboard');
+        }
 
-        // return redirect()->back()->withInput($request->only('email','remember'));
+        return redirect()->back()->withInput($request->only('email','remember'));
 
-    //     return '';
-    // }
+        return '';
+    }
     
-// }
+}
--- app/Http/Controllers/userProfileController.php
+++ app/Http/Controllers/userProfileController.php
@@ -17,11 +17,7 @@ class userProfileController extends Controller
      *
      * @return void
      */
-    // public function updateUserProfile()
-    // {
-    //     return view('updateUserProfile');
-    // }
-
+  
     public function editUserProfile()
     {
         $email = Auth::user()->email;
@@ -47,36 +43,6 @@ class userProfileController extends Controller
         $customer->save();
 
         return redirect()->back()->with("success","Profile Updated Successfully !");
-
-        // $customers = DB::table('customers');
-        // $email = Auth::user()->email;
-
-        // $customer = Customer::find($email);
-
-        // $customers->update([
-        //     'fname' => $request['fname'],
-        //     'lname' => $request['lname'],
-        //     'no' => $request['no'],
-        //     'street' => $request['street'],
-        //     'city' => $request['city'],
-        //     'phoneNo' => $request['phoneNo'],
-        //     'nic' => $request['nic'],
-        //     'email' => $request['email'],
-        // ])->except(['_token']);
-        // return redirect('updateUserProfile')->with('success','Product updated successfully');
-
-        // dd($request->all());
-        // $customer->fname = $request->input('fname');
-        // $customer->lname = $request->input('lname');
-        // $customer->no = $request->input('no');
-        // $customer->street = $request->input('street');
-        // $customer->city = $request->input('city');
-        // $customer->phoneno = $request->input('phoneno');
-        // $customer->nic = $request->input('nic');
-        // $customer->email = $request->input('email');
-        // $customer->save();
-  
-        // return redirect('updateUserProfile')->with('success', 'Stock has been updated');
     }
   
 }
