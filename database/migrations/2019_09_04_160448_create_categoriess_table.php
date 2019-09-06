ec076e724daf9433631ded23699e0b6fc2b12ce8
sandaru imal <sandaruimal99@gmail.com>
2019-09-06 09:59:54 +0530

    updated set value

--- database/migrations/2019_09_04_160448_create_categoriess_table.php
+++ database/migrations/2019_09_04_160448_create_categoriess_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCategoriessTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('userId');
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->text('description');
+            $table->string('url');
+            $table->tinyInteger('status');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -30,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categoriess');
     }
 }
